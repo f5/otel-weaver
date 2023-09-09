ec13d930990316815578ac7ae868fe59d39d13cc
querel <l.querel@f5.com>
2023-09-09 14:04:38 -0700

    update cli command languages

--- src/languages.rs
+++ src/languages.rs
@@ -3,12 +3,9 @@
 //! List of supported languages
 
 use std::path::PathBuf;
-use std::process::exit;
 use clap::Parser;
 
 use logger::Logger;
-use resolver::SchemaResolver;
-use crate::resolve::ResolveParams;
 
 /// Parameters for the `languages` command
 #[derive(Parser)]
@@ -22,11 +19,22 @@ pub struct LanguagesParams {
 pub fn command_languages(log: &mut Logger, params: &LanguagesParams) {
     /// List all directories in the templates directory
     log.log("List of supported languages:");
-    for entry in std::fs::read_dir(&params.templates).expect("Failed to read templates directory") {
-        let entry = entry.expect("Failed to read template directory entry");
-        if entry.file_type().expect("Failed to read file type").is_dir() {
-            log.indent(1);
-            log.log(&format!("- {}", entry.file_name().to_str().unwrap()));
+    let template_dir = match std::fs::read_dir(&params.templates) {
+        Ok(dir) => dir,
+        Err(e) => {
+            log.error(&format!("Failed to read templates directory: {}", e));
+            std::process::exit(1);
+        }
+    };
+    for entry in template_dir {
+        if let Ok(entry) = entry {
+            if entry.file_type().is_ok() {
+                log.indent(1);
+                log.log(&format!("- {}", entry.file_name().to_str().unwrap()));
+            }
+        } else {
+            log.error("Failed to read template directory entry");
+            std::process::exit(1);
         }
     }
 }
